db675500340d1ed0d1a986c941d6720333aea23f
@ThibaultTricard <thibault.tricard@hotmail.fr>
2021-01-18 11:51:15 +0100

    SSAO

--- Samples/Source Files/SSAO/main.cpp
+++ Samples/Source Files/SSAO/main.cpp
@@ -10,11 +10,16 @@ using namespace LavaCake::Framework;
 using namespace LavaCake::Core;
 
 
+float lerp(float a, float b, float f)
+{
+	return a + f * (b - a);
+}
+
 int main() {
 
 
 	Window w("LavaCake : SSAO", 1280, 720);
-	//glfwSetWindowPos(w.m_window, 2000, 100);
+	glfwSetWindowPos(w.m_window, 2000, 100);
 	Mouse* mouse = Mouse::getMouse();
 	Device* d = Device::getDevice();
 	d->initDevices(0, 1, w.m_windowParams);
@@ -62,12 +67,12 @@ int main() {
 
 	
 	SubpassAttachment SA;
-	SA.showOnScreen = true;
+	SA.showOnScreen = false;
 	SA.nbColor = 3;
 	SA.storeColor = true;
 	SA.useDepth = true;
 	SA.storeDepth = true;
-	SA.showOnScreenIndex = 1;
+	SA.showOnScreenIndex = 2;
 
 	Gpass.addSubPass({Gpipeline}, SA);
 	Gpass.addDependencies(VK_SUBPASS_EXTERNAL, 0, VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT, VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT, VK_ACCESS_MEMORY_READ_BIT, VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT, VK_DEPENDENCY_BY_REGION_BIT);
@@ -83,6 +88,88 @@ int main() {
 	//																					SSAO																											//
 	////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+
+
+	Mesh_t* quadMesh = new IndexedMesh<TRIANGLE>(P3UV);
+
+	quadMesh->appendVertex({ -1, -1, 0, 0, 0 });
+	quadMesh->appendVertex({ -1, 1, 0, 0, 1 });
+	quadMesh->appendVertex({ 1, 1, 0, 1, 1 });
+	quadMesh->appendVertex({ 1, -1, 0, 1, 0 });
+
+	quadMesh->appendIndex(0);
+	quadMesh->appendIndex(1);
+	quadMesh->appendIndex(2);
+
+	quadMesh->appendIndex(2);
+	quadMesh->appendIndex(3);
+	quadMesh->appendIndex(0);
+
+	VertexBuffer* quadBuffer = new VertexBuffer({quadMesh});
+	quadBuffer->allocate(graphicQueue, cmdBuf);
+
+
+	UniformBuffer* SSAOuni = new UniformBuffer();
+
+	std::vector<Data*> samples;
+	srand(time(NULL));
+	for (unsigned int i = 0; i < 64; i++)
+	{
+		vec3f noise({
+			(float)rand() / (float)RAND_MAX * 2.0f - 1.0f,
+			(float)rand() / (float)RAND_MAX * 2.0f - 1.0f,
+			(float)rand() / (float)RAND_MAX });
+
+		noise = Normalize(noise) * ((float)rand() / (float)RAND_MAX);
+		
+		float scale = (float)i / 64.0;
+		scale = lerp(0.1f, 1.0f, scale * scale);
+		noise =noise * scale;
+
+		samples.push_back(new vec3f({ noise[0], noise[1], noise[2] }));
+	}
+
+	
+
+	SSAOuni->addVariable("samples", &samples);
+	SSAOuni->addVariable("projection", &proj);
+
+	SSAOuni->end();
+
+
+	RenderPass* SSAORenderPass = new RenderPass();
+
+	GraphicPipeline* SSAOpipeline = new GraphicPipeline(vec3f({ 0,0,0 }), vec3f({ float(size.width),float(size.height),1.0f }), vec2f({ 0,0 }), vec2f({ float(size.width),float(size.height) }));
+
+	VertexShaderModule* SSAOvertex = new VertexShaderModule("Data/Shaders/SSAO/SSAO.vert.spv");
+	FragmentShaderModule* SSAOfragment = new FragmentShaderModule("Data/Shaders/SSAO/SSAO.frag.spv");
+
+	SSAOpipeline->setVertices(quadBuffer);
+
+	SSAOpipeline->addFrameBuffer(G, VK_SHADER_STAGE_FRAGMENT_BIT, 0, 0);
+	SSAOpipeline->addFrameBuffer(G, VK_SHADER_STAGE_FRAGMENT_BIT, 1, 1);
+	SSAOpipeline->addFrameBuffer(G, VK_SHADER_STAGE_FRAGMENT_BIT, 2, 2);
+
+	SSAOpipeline->addUniformBuffer(SSAOuni, VK_SHADER_STAGE_FRAGMENT_BIT, 4);
+
+	SSAOpipeline->setVextexShader(SSAOvertex);
+	SSAOpipeline->setFragmentModule(SSAOfragment);
+	//SSAOpipeline->SetCullMode();
+
+	SubpassAttachment SSAOattachment;
+	SSAOattachment.showOnScreen = true;
+	SSAOattachment.nbColor = 1;
+	SSAOattachment.storeColor = true;
+	SSAOattachment.useDepth = true;
+	SSAOattachment.storeDepth = false;
+	SSAOattachment.showOnScreenIndex = 0;
+
+	SSAORenderPass->addSubPass({ SSAOpipeline }, SSAOattachment);
+	SSAORenderPass->compile();
+
+
+	FrameBuffer* SSAO = new FrameBuffer(size.width, size.height);
+	SSAORenderPass->prepareOutputFrameBuffer(*SSAO);
 
 
 
@@ -160,13 +247,18 @@ int main() {
 
 		if (updateUniform) {
 			b->update(cmdBuf.getHandle());
+			SSAOuni->update(cmdBuf.getHandle());
 			updateUniform = false;
 		}
 
 
-		Gpass.setSwapChainImage(*G, swapChainImage);
+		
 
 		Gpass.draw(cmdBuf.getHandle(), G->getHandle(), vec2u({ 0,0 }), vec2u({ size.width, size.height }), { { 0.0f, 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 0.0f, 0.0f }, { 1.0f, 0.0f } });
+
+
+		SSAORenderPass->setSwapChainImage(*SSAO, swapChainImage);
+		SSAORenderPass->draw(cmdBuf.getHandle(), SSAO->getHandle(), vec2u({ 0,0 }), vec2u({ size.width, size.height }), { { 0.0f, 0.0f, 0.0f, 0.0f }, { 1.0f, 0.0f } });
 
 		cmdBuf.endRecord();
 
